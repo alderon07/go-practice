49d75ecc2a7cf1dc62c966b04751b9ebfa10e247
Naqi <naqihaider07@gmail.com>
2024-07-25 20:50:40 -0400

    doing some stuff

--- api/types.go
+++ api/types.go
@@ -1,20 +1,19 @@
 package main
 
-
 type Response struct {
-	Message string `json:"message"`
-	Status  string  `json:"status"`
-	Data   []Player `json:"data"`
+    Message string   `json:"message"`
+    Status  string   `json:"status"`
+    Data    []Player `json:"data"`
 }
 
 type Player struct {
-	ID 				string 	`json:"id"`
-	Name 			string 	`json:"name"`
-	JerseyNumber 	int8 	`json:"jersey_number"`
-	Rating 			int8 	`json:"rating"`
+    ID           string `json:"id"`
+    Name         string `json:"name"`
+    JerseyNumber int8   `json:"jersey_number"`
+    Rating       int8   `json:"rating"`
 }
 
 func (p *Player) Update(player Player) {
-	p.JerseyNumber = player.JerseyNumber
-	p.Rating = player.Rating
+    p.JerseyNumber = player.JerseyNumber
+    p.Rating = player.Rating
 }
