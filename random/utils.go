fba8a0fe006b985fb3930da060f5378cdbff3df4
Naqi <naqihaider07@gmail.com>
2024-07-25 20:51:16 -0400

    doing more stuff

--- random/utils.go
+++ random/utils.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"errors"
+    "errors"
 )
 
 // Number is a type constraint that allows any of the specified numeric types.
 type Number interface {
-	uint64 | uint32 | uint16 | uint8 | int64 | int32 | int16 | int8 | float32 | float64
+    uint64 | uint32 | uint16 | uint8 | int64 | int32 | int16 | int8 | float32 | float64
 }
 
 // SumArray calculates the sum of the elements in the provided array.
@@ -19,22 +19,22 @@ type Number interface {
 // - The sum of the elements in the array.
 // - An error if the array is empty.
 func SumArray[T Number](array []T) (T, error) {
-	var sum T
+    var sum T
 
-	if len(array) == 0 {
-		return 0, errors.New("the array is empty")
-	}
+    if len(array) == 0 {
+        return 0, errors.New("the array is empty")
+    }
 
-	if len(array) == 1 {
-		sum = array[0]
-		return sum, nil
-	}
+    if len(array) == 1 {
+        sum = array[0]
+        return sum, nil
+    }
 
-	for _, elem := range array {
-		sum += elem
-	}
+    for _, elem := range array {
+        sum += elem
+    }
 
-	return sum, nil
+    return sum, nil
 }
 
 // Map applies a given function to each element of the provided array and returns a new array with the results.
@@ -47,18 +47,14 @@ func SumArray[T Number](array []T) (T, error) {
 // Returns:
 // - A new slice with the results of applying the function to each element of the original array.
 // - An error if the array is empty.
-func Map[T any](array []T, f func(T) T) ([]T, error) {
-	var result []T
+func Map[T any, P any](values []T, f func(T) P) []P {
+    res := make([]P, 0, len(values))
 
-	if len(array) == 0 {
-		return nil, errors.New("the array is empty")
-	}
+    for _, v := range values {
+        res = append(res, f(v))
+    }
 
-	for _, elem := range array {
-		result = append(result, f(elem))
-	}
-
-	return result, nil
+    return res
 }
 
 // Filter returns a new array containing only the elements of the provided array that satisfy the given predicate function.
@@ -72,17 +68,17 @@ func Map[T any](array []T, f func(T) T) ([]T, error) {
 // - A new slice with the elements that satisfy the predicate function.
 // - An error if the array is empty.
 func Filter[T any](array []T, f func(T) bool) ([]T, error) {
-	var result []T
+    var result []T
 
-	if len(array) == 0 {
-		return nil, errors.New("the array is empty")
-	}
+    if len(array) == 0 {
+        return nil, errors.New("the array is empty")
+    }
 
-	for _, elem := range array {
-		if f(elem) {
-			result = append(result, elem)
-		}
-	}
+    for _, elem := range array {
+        if f(elem) {
+            result = append(result, elem)
+        }
+    }
 
-	return result, nil
+    return result, nil
 }
